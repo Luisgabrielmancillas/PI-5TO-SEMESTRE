eb16e0d8a511fa60978bab4df43ed2caae39848f
Manuel Valencia <mvalencia18@ucol.mx>
2025-11-23 16:00:03 -0600

    Build

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,12 @@ use App\Http\Controllers\Api\SeleccionHortalizaApiController;
 
 
 
-Route::post('/login', [UserApiController::class, 'login']);
-Route::get('/user', [UserApiController::class, 'me']);
-Route::get('/actuadores', [DataApiController::class, 'actuadores']);
-Route::get('/config-sensores', [DataApiController::class, 'configSensores']);
-Route::get('/nutrientes', [DataApiController::class, 'nutrientes']);
-Route::get('/registro-mediciones', [DataApiController::class, 'registroMediciones']);
-Route::get('/seleccion-hortalizas', [DataApiController::class, 'seleccionHortalizas']);
-Route::get('/sensores', [DataApiController::class, 'sensores']);
-Route::get('/hortaliza/actual', [SeleccionHortalizaApiController::class, 'get']);
-Route::post('/hortaliza/cambiar', [SeleccionHortalizaApiController::class, 'set']);
+Route::post('login', [UserApiController::class, 'login']);
+Route::get('actuadores', [DataApiController::class, 'actuadores']);
+Route::get('config-sensores', [DataApiController::class, 'configSensores']);
+Route::get('nutrientes', [DataApiController::class, 'nutrientes']);
+Route::get('registro-mediciones', [DataApiController::class, 'registroMediciones']);
+Route::get('seleccion-hortalizas', [DataApiController::class, 'seleccionHortalizas']);
+Route::get('sensores', [DataApiController::class, 'sensores']);
+Route::get('hortaliza/actual', [SeleccionHortalizaApiController::class, 'get']);
+Route::post('hortaliza/cambiar', [SeleccionHortalizaApiController::class, 'set']);
