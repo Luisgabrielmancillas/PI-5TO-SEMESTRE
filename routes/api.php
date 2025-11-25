35e7659ec6892ae4a51ed90b619e9c470a0b9c13
Manuel Valencia <mvalencia18@ucol.mx>
2025-11-25 09:47:28 -0600

    Prueba de funcionamiento de la web

--- routes/api.php
+++ routes/api.php
@@ -9,16 +9,16 @@ use App\Http\Controllers\Api\RegistroActuadorApiController;
 
 
 
-Route::post('login', [UserApiController::class, 'login']);
-Route::get('actuadores', [DataApiController::class, 'actuadores']);
-Route::get('config-sensores', [DataApiController::class, 'configSensores']);
-Route::get('nutrientes', [DataApiController::class, 'nutrientes']);
-Route::get('registro-mediciones', [DataApiController::class, 'registroMediciones']);
-Route::get('seleccion-hortalizas', [DataApiController::class, 'seleccionHortalizas']);
-Route::get('sensores', [DataApiController::class, 'sensores']);
-Route::get('hortaliza/actual', [SeleccionHortalizaApiController::class, 'get']);
-Route::post('hortaliza/cambiar', [SeleccionHortalizaApiController::class, 'set']);
+Route::post('/login', [UserApiController::class, 'login']);
+Route::get('/actuadores', [DataApiController::class, 'actuadores']);
+Route::get('/config-sensores', [DataApiController::class, 'configSensores']);
+Route::get('/nutrientes', [DataApiController::class, 'nutrientes']);
+Route::get('/registro-mediciones', [DataApiController::class, 'registroMediciones']);
+Route::get('/seleccion-hortalizas', [DataApiController::class, 'seleccionHortalizas']);
+Route::get('/sensores', [DataApiController::class, 'sensores']);
+Route::get('/hortaliza/actual', [SeleccionHortalizaApiController::class, 'get']);
+Route::post('/hortaliza/cambiar', [SeleccionHortalizaApiController::class, 'set']);
 
-Route::post('actuador/estado', [RegistroActuadorApiController::class, 'estado']);
-Route::post('actuador/encender', [RegistroActuadorApiController::class, 'encender']);
-Route::post('actuador/apagar', [RegistroActuadorApiController::class, 'apagar']);
+Route::post('/actuador/estado', [RegistroActuadorApiController::class, 'estado']);
+Route::post('/actuador/encender', [RegistroActuadorApiController::class, 'encender']);
+Route::post('/actuador/apagar', [RegistroActuadorApiController::class, 'apagar']);
